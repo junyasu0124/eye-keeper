50ef4fded4585c28e4176d14280c90e32d1bffa2
junyasu <jumpeiyasue@gmail.com>
2024-04-03 12:17:28 +0900

    feat: Added navigation

--- EyeKeeper/MainWindow.xaml.cs
+++ EyeKeeper/MainWindow.xaml.cs
@@ -1,25 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace EyeKeeper;
-/// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class MainWindow : Window
 {
   public MainWindow()
@@ -27,8 +9,13 @@ public sealed partial class MainWindow : Window
     this.InitializeComponent();
   }
 
-  private void myButton_Click(object sender, RoutedEventArgs e)
+  private void navigation_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
   {
-    myButton.Content = "Clicked";
+
+  }
+
+  private void contentFrame_Navigated(object sender, Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
+  {
+
   }
 }
